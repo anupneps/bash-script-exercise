e749f539cae23c772606cce7ea16ef296b7314f8
Anup Nepal <anup.neps@outlook.com>
2022-01-24 18:32:50 +0200

    lesson ex

---
+++ myJS.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    alert("Something has been done!");
+}
+
+function buttonFunction() {
+    alert("The button is pressed by the event listener!!!")
+}
+
+function inputFunction() {
+    alert("input something")
+
+}
