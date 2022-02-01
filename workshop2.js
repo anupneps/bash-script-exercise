699df711118255e66aa2f3ab5fae144905033001
Anup Nepal <anup.neps@outlook.com>
2022-02-01 12:33:44 +0200

    workshop2

---
+++ workshop2.js
@@ -0,0 +1,11 @@
+//Exercise 1: writing in document //
+//Function to repeat the text.//
+
+// var nav = navigator.appname;
+
+// if (nav == "Mozilla") {
+//     location.href = "http://www.mozilla.org";
+// } else {
+//     location.href = "https://www.google.com/"
+
+// }
